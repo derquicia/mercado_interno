d69b92cf189e2067b4812410d593369d791ca125
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-07 16:57:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,20 +21,20 @@ st_echarts(options=options, renderer="svg")
 
 
  with st.echo("below"):
-        b = (
-            Bar()
-            .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
-            .add_yaxis(
-                "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
-            )
-            .set_global_opts(
-                title_opts=opts.TitleOpts(
-                    title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
-                ),
-                toolbox_opts=opts.ToolboxOpts(),
-            )
+     b = (
+        Bar()
+        .add_xaxis(["Microsoft", "Amazon", "IBM", "Oracle", "Google", "Alibaba"])
+        .add_yaxis(
+            "2017-2018 Revenue in (billion $)", [21.2, 20.4, 10.3, 6.08, 4, 2.2]
         )
-    st_pyecharts(b)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(
+                title="Top cloud providers 2018", subtitle="2017-2018 Revenue"
+            ),
+            toolbox_opts=opts.ToolboxOpts(),
+        )
+    )
+st_pyecharts(b)
 
 
 
