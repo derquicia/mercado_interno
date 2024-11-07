83e354bfdc50b5fd0a047ad61f54d31f99cfb673
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-07 17:01:22 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from streamlit_echarts import st_echarts
 from streamlit_echarts import JsCode
 from streamlit_echarts import st_pyecharts
 from pyecharts.charts import Bar
+from pyecharts import options as opts
 
 
 options = {
