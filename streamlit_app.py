42bcba32aa541353cb097b937f1d49df55d4c2bf
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-07 14:42:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ df = conn.query('SELECT periodo,"CERVEZAS","VINOS_COMUNES" FROM scentia_res;', t
 st.write(df)
 
 
-st.line_chart(df,x=df.periodo,y=df.CERVEZAS)
+st.line_chart(df)
 
 # st.write(df)
 st.bar_chart(df)
