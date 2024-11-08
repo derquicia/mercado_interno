c7dc2a2a093f9cf1c4fa60935b0a88530a1910e3
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-08 13:34:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ st.line_chart(df,x="periodo",y=["CERVEZAS","VINOS_COMUNES","VINOS_FINOS"])
 st.dataframe(df)
 option = {
     "xAxis": {
-        "type": Time_Created,
+        "type": perido,
         data: [periodo],
     },
     "yAxis": {"type": censored},
