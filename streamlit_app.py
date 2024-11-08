b9169ebc7caa1344e35e8069346f25bd419a396a
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-08 15:06:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import json
 from streamlit_echarts import st_echarts
 from streamlit_echarts import JsCode
 from streamlit_echarts import st_pyecharts
