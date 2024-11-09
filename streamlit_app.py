5af3f0bed0ae647b8e85af4826af2cae807220b7
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-09 12:05:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,6 @@ from pyecharts.charts import Bar
 from pyecharts import options as opts
 
 
-
-
-
 conn = st.connection("postgresql", type="sql")
 df = conn.query('SELECT periodo,"CERVEZAS","VINOS_COMUNES","VINOS_FINOS","APERITIVOS_ALC","APERITIVOS_RTD","ESPUMANTES","FRIZANTES","SIDRAS_Y_SABORES","VINOS_FORTIFICADOS" FROM scentia_res;', ttl="0")
 #st.write(df)
