701addd9f68ce7b585276fc2857bed3bba0e3ccb
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-09 12:59:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,7 +183,7 @@ json_list = json.loads(json.dumps(list(df3.T.to_dict().values())))
 #diskData = json.loads(temp.json)
 st.write('convert')
 #st.write(diskData)
-#st.write(json_list)
+st.write(json_list)
 
 
 option = {
