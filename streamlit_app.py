3f10205519887023a3dc2b821544e040e117cc35
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-12 17:18:01 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,10 @@ from pyecharts.charts import Bar
 from pyecharts import options as opts
 
 
+[data-testid="stStatusWidget"] {
+    visibility: hidden;
+}
+
 hide_st_style = """ 
     <style>
     #MainMenu {visibilty: hidden;}
