afdd82880034ac45c5bc3642d40c969fe172f2c8
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-13 14:29:52 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -8,3 +8,35 @@ from streamlit_echarts import JsCode
 from streamlit_echarts import st_pyecharts
 from pyecharts.charts import Bar
 from pyecharts import options as opts
+
+conn = st.connection("postgresql", type="sql")
+df = conn.query('select anio,sum(cantlitros) litros,sum(valorfobsolo) fob from exportaciones2_m where producto  not in ('Mosto','Alcohol') group by anio;', ttl="0")
+#st.write(df)
+
+st.subheader('Evolución Exportaciones de vimos por año')
+
+if st.checkbox('Ver datos en forma de tabla'):
+    st.write(df)
+
+
+df['anio'] = df['anio'].astype(str)
+
+newdf=df.set_index('anio',inplace=False).rename_axis(None)
+
+option = {
+    "tooltip": {
+        "trigger": 'axis',
+        "axisPointer": { "type": 'cross' }
+    },
+    "legend": {},    
+    "xAxis": {
+        "type": "category",
+        "data": df['periodo'].to_list(),
+    },
+    "yAxis": {"type": "value"},
+    "series": [{"data": df['litros'].to_list(), "type": "line", "name": 'Litros'}
+               ,{"data": df['fob'].to_list(), "type": "line","name":'Fob'}
+}
+st_echarts(
+    options=option, height="400px",
+)
