c8e12b3433eaac06d4079497e78d54d2a3ad77d7
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-13 14:57:50 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -12,30 +12,3 @@ conn = st.connection("postgresql", type="sql")
 df = conn.query('select anio,litros,fob from inf_expo_anio ;', ttl="0")
 #st.write(df)
  
-st.subheader('Evolución Exportaciones de vimos por año')
-
-if st.checkbox('Ver datos en forma de tabla'):
-    st.write(df)
-
-
-df['anio'] = df['anio'].astype(str)
-
-newdf=df.set_index('anio',inplace=False).rename_axis(None)
-
-option = {
-    "tooltip": {
-        "trigger": 'axis',
-        "axisPointer": { "type": 'cross' }
-    },
-    "legend": {},    
-    "xAxis": {
-        "type": "category",
-        "data": df['anio'].to_list(),
-    },
-    "yAxis": {"type": "value"},
-    "series": [{"data": df['litros'].to_list(), "type": "line", "name": 'Litros'}
-               ,{"data": df['fob'].to_list(), "type": "line","name":'Fob'}]
-}
-st_echarts(
-    options=option, height="400px",
-)
