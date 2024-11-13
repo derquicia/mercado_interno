85e1db8b79515e970df1f480da1bfb2ea89860f4
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-13 16:44:16 -0300

    Update 🌍Exportaciones.py

--- pages/🌍Exportaciones.py
+++ pages/🌍Exportaciones.py
@@ -72,3 +72,44 @@ option = {
 st_echarts(
     options=option, height="400px" ,
 )
+
+df2 = conn.query('select variedad1,litros,fob from info_expo_anio_variedad ;', ttl="0")
+#st.write(df1)
+ 
+st.subheader('Exportaciones por Variedad')
+
+
+option = {
+        "title": {
+            "text": "WORLD COFFEE RESEARCH SENSORY LEXICON",
+            "subtext": "Source: https://worldcoffeeresearch.org/work/sensory-lexicon/",
+            "textStyle": {"fontSize": 14, "align": "center"},
+            "subtextStyle": {"align": "center"},
+            "sublink": "https://worldcoffeeresearch.org/work/sensory-lexicon/",
+        },
+        "series": {
+            "type": "sunburst",
+            "data": data,
+            "radius": [0, "95%"],
+            "sort": None,
+            "emphasis": {"focus": "ancestor"},
+            "levels": [
+                {},
+                {
+                    "r0": "15%",
+                    "r": "35%",
+                    "itemStyle": {"borderWidth": 2},
+                    "label": {"rotate": "tangential"},
+                },
+                {"r0": "35%", "r": "70%", "label": {"align": "right"}},
+                {
+                    "r0": "70%",
+                    "r": "72%",
+                    "label": {"position": "outside", "padding": 3, "silent": False},
+                    "itemStyle": {"borderWidth": 3},
+                },
+            ],
+        },
+    }
+st_echarts(option, height="700px")
+
