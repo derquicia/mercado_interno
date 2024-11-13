757465d069530af5227ed0cae6bae3fbafb6e846
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-13 13:03:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ def main():
     # builds the sidebar menu
     with st.sidebar:
         st.page_link('streamlit_app.py', label='Individual Checker', icon='🔥')
-        st.page_link('pages/competition.py', label='Competition Checker', icon='🛡️')
 
     st.title(f'🛡️ Competition Checker')
 
