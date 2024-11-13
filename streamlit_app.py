07868e6d4f8796817fb52f721e50b7be6c2378c9
derquicia <derquicia@mariogoldsteinsa.com.ar>
2024-11-13 14:00:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def page2():
     st.title("Second page")
 
 pg = st.navigation([
-    st.Page("\pages\Exportaciones.py", title="First page", icon="🔥"),
+    st.Page("/pages/Exportaciones.py", title="First page", icon="🔥"),
     st.Page(page2, title="Second page", icon=":material/favorite:"),
 ])
 pg.run()
